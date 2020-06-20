4bdb28cae79a28846462586e5f75d84d38b0608c
Rejke <Rejke@yandex.ru>
2020-06-20 19:24:22 +0300

    Added menu options

--- app/src/main/java/com/example/todoapp/NewTodo.java
+++ app/src/main/java/com/example/todoapp/NewTodo.java
@@ -1,8 +1,12 @@
 package com.example.todoapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class NewTodo extends AppCompatActivity {
@@ -13,5 +17,15 @@ public class NewTodo extends AppCompatActivity {
         setContentView(R.layout.activity_new_todo);
 
         ((TextView) findViewById(R.id.textSeparator)).setText("Категория");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_options, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
