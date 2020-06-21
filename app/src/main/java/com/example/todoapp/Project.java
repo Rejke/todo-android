e2ad3c279b8d0ba5a49eebc0148aabdd7932dfe6
Rejke <Rejke@yandex.ru>
2020-06-21 11:38:36 +0300

    Made Project model parcelable

--- app/src/main/java/com/example/todoapp/Project.java
+++ app/src/main/java/com/example/todoapp/Project.java
@@ -1,10 +1,11 @@
 package com.example.todoapp;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
-import java.util.List;
-
-public class Project {
+public class Project implements Parcelable {
     @SerializedName("id")
     private int id;
     @SerializedName("title")
@@ -25,4 +26,32 @@ public class Project {
     public void setTitle(String title) {
         this.title = title;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(id);
+        parcel.writeString(title);
+    }
+
+    protected Project(Parcel in) {
+        id = in.readInt();
+        title = in.readString();
+    }
+
+    public static final Creator<Project> CREATOR = new Creator<Project>() {
+        @Override
+        public Project createFromParcel(Parcel in) {
+            return new Project(in);
+        }
+
+        @Override
+        public Project[] newArray(int size) {
+            return new Project[size];
+        }
+    };
 }
